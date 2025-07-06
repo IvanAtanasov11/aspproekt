7158dc0203df5dbbdfdf1031e294cf770b32db9b
denislav <rockstar20detetochudo@gmail.com>
2025-07-06 23:29:21 +0300

    asd

---
+++ RonaldoSUI/Controllers/RonaldoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using GrigorDimitrovProfile.Models;
+
+public class RonaldoController : Controller
+{
+    public IActionResult Index()
+    {
+        var profile = new Athlete
+        {
+            Name = "Кристиано Роналдо",
+            BirthDate = new DateTime(1985, 2, 5),
+            Country = "Португалия",
+            Sport = "Футбол",
+            Ranking = 1,
+            Biography = "Кристиано Роналдо е един от най-великите футболисти в историята, с пет Златни топки и стотици голове в клубната и националната сцена."
+        };
+
+        return View(profile);
+    }
+
+    [Authorize]
+    public IActionResult Details()
+    {
+        var details = new Athlete
+        {
+            Name = "Кристиано Роналдо",
+            BirthDate = new DateTime(1985, 2, 5),
+            Country = "Португалия",
+            Sport = "Футбол",
+            Ranking = 1,
+            Biography = "Кариерата на Роналдо включва отбори като Спортинг Лисабон, Манчестър Юнайтед, Реал Мадрид, Ювентус и Ал-Наср. Известен с невероятна физика, скорост и голов нюх."
+        };
+
+        return View(details);
+    }
+}
---
+++ RonaldoSUI/Models/Athlete.cs
@@ -0,0 +1,12 @@
+﻿namespace GrigorDimitrovProfile.Models
+{
+    public class Athlete
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Country { get; set; }
+        public string Sport { get; set; }
+        public int Ranking { get; set; }
+        public string Biography { get; set; }
+    }
+}
